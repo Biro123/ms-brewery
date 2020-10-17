335145988b96a7891c55920c5bca61c7bfba97b4
Biro123 <biro123@ntlworld.com>
2020-10-17 15:46:42 +0100

    Added customer endpoints

--- src/main/java/com/oldman/msbrewery/services/CustomerServiceImpl.java
+++ src/main/java/com/oldman/msbrewery/services/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.oldman.msbrewery.services;
 
 import com.oldman.msbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
@@ -13,5 +15,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("BeerMen")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer..");
     }
 }
