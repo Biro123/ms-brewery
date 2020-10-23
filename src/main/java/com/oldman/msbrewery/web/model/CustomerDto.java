d567954c60b934dd72fd59928e14e067079ade9f
Biro123 <biro123@ntlworld.com>
2020-10-23 21:17:41 +0100

    Tweak CustomerDto validation

--- src/main/java/com/oldman/msbrewery/web/model/CustomerDto.java
+++ src/main/java/com/oldman/msbrewery/web/model/CustomerDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.UUID;
 
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class CustomerDto {
     private UUID id;
 
-    @NotNull
+    @NotBlank
     @Size(min=3, max=100)
     private String name;
 }
