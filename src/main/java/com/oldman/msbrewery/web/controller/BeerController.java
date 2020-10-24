1e72f4ad712a9d54aea321cda06f9d5fabf7f683
Biro123 <biro123@ntlworld.com>
2020-10-24 16:40:29 +0100

    Add a bit more Lombok to BeerController

--- src/main/java/com/oldman/msbrewery/web/controller/BeerController.java
+++ src/main/java/com/oldman/msbrewery/web/controller/BeerController.java
@@ -2,34 +2,34 @@ package com.oldman.msbrewery.web.controller;
 
 import com.oldman.msbrewery.services.BeerService;
 import com.oldman.msbrewery.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    public BeerController(BeerService beerService) {
-        this.beerService = beerService;
-    }
-
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
-    @PostMapping  // create
+    @PostMapping  // POST - Create new beer
     public ResponseEntity handlePost (@Valid @RequestBody BeerDto beerDto) {
+
+        log.info("In handle post slf4j log...");
+
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
@@ -39,7 +39,7 @@ public class BeerController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{beerId}")  // update or create
+    @PutMapping("/{beerId}")  // PUT - update or create
     public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
