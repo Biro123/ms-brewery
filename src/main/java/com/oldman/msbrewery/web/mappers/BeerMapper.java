229068c4d2e63607dab2d4a14b82d20a84e640b9
Biro123 <biro123@ntlworld.com>
2020-10-25 15:32:15 +0000

    Create date mapper and add to mapStruct

--- src/main/java/com/oldman/msbrewery/web/mappers/BeerMapper.java
+++ src/main/java/com/oldman/msbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.oldman.msbrewery.domain.Beer;
 import com.oldman.msbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper( uses = DateMapper.class )
 public interface BeerMapper {
     BeerDto beerToBeerDto(Beer beer);
     Beer beerDtoToBeer(BeerDto dto);
---
+++ src/main/java/com/oldman/msbrewery/web/mappers/DateMapper.java
@@ -0,0 +1,27 @@
+package com.oldman.msbrewery.web.mappers;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class DateMapper {
+
+    public OffsetDateTime asOffsetDateTime(Timestamp ts) {
+        if (ts != null) {
+            return OffsetDateTime.of(ts.toLocalDateTime().getYear(), ts.toLocalDateTime().getMonthValue(),
+                    ts.toLocalDateTime().getDayOfMonth(), ts.toLocalDateTime().getHour(), ts.toLocalDateTime().getMinute(),
+                    ts.toLocalDateTime().getSecond(), ts.toLocalDateTime().getNano(), ZoneOffset.UTC);
+        } else {
+            return null;
+        }
+    }
+
+    public Timestamp asTimeStamp(OffsetDateTime odt) {
+        if(odt != null) {
+            return Timestamp.valueOf(odt.atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime());
+        } else {
+            return null;
+        }
+    }
+
+}
